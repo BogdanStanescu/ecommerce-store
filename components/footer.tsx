8da937acfb94ea588db5b5f0c19644f55d8a5313
Stanescu Bogdan <bogdan.stanescu96@gmail.com>
2023-11-18 16:55:52 +0200

    feat: footer component

---
+++ components/footer.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="w-full bg-white border-t absolute bottom-0 right-0">
+      <div className="mx-auto py-10">
+        <p className="text-center text-xs text-black">
+          &copy; 2023 E-Commerce Store. All rights reserved. Powered by{" "}
+          <a
+            href="https://www.linkedin.com/in/bogdanstanescu/"
+            target="_blank"
+            className="underline font-bold text-blue-950"
+          >
+            Stănecsu Bogdan 🚀
+          </a>
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
